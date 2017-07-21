801f7376ad3f5b39bbbe9e377914e0af061259d2
Daniel Watkins <daniel.watkins@canonical.com>
2017-07-21 13:52:11 +0200

    Rename variables to be less generic
    This is prep for a following commit, but still useful to have
    separately.

--- metrics/cloud_images.py
+++ metrics/cloud_images.py
@@ -29,29 +29,30 @@ def parse_simplestreams_for_images(cloud_name, image_type):
     """Use sstream-query to fetch supported image information."""
     url = URL_PATTERNS[image_type].format(cloud_name=cloud_name)
     output = subprocess.check_output(['sstream-query', '--json', url])
-    data = defaultdict(lambda: defaultdict(int))
+    image_counts = defaultdict(lambda: defaultdict(int))
     for product_dict in json.loads(output.decode('utf-8')):
-        data[product_dict['release']][product_dict['arch']] += 1
-    return data
+        image_counts[product_dict['release']][product_dict['arch']] += 1
+    return image_counts
 
 
 def collect(dryrun=False):
     """Push published cloud image counts."""
     registry = CollectorRegistry()
-    gauge = Gauge('foundations_cloud_images_published', '',
-                  ['image_type', 'cloud', 'release', 'arch'],
-                  registry=registry)
+    count_gauge = Gauge('foundations_cloud_images_published', '',
+                        ['image_type', 'cloud', 'release', 'arch'],
+                        registry=registry)
     for image_type in ['daily', 'release']:
         for cloud_name in CLOUD_NAMES[image_type]:
             print('Counting {} images for {}...'.format(image_type,
                                                         cloud_name))
-            data = parse_simplestreams_for_images(cloud_name, image_type)
-            for release in data:
-                for arch in data[release]:
-                    count = data[release][arch]
+            image_counts = parse_simplestreams_for_images(cloud_name,
+                                                          image_type)
+            for release in image_counts:
+                for arch in image_counts[release]:
+                    count = image_counts[release][arch]
                     print('Found {} {} images for {} {} {}'.format(
                         count, image_type, cloud_name, release, arch))
-                    gauge.labels(
+                    count_gauge.labels(
                         image_type, cloud_name, release, arch).set(count)
 
     if not dryrun:
